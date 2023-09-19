1cff69a8273c9f14d218101da41d899fd65341a1
Sayed-Mersal <sayedmersal63@gmail.com>
2023-09-19 15:31:35 -0700

    main.h file upload

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int print_char(va_list ap);
+int print_string(va_list ap);
+int print_percent(va_list ap);
+
+endif /* MAIN_H */
