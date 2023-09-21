0a49b0eb23dd7c6a824a72883a971296ec66ad8f
Sayed-Mersal <sayedmersal63@gmail.com>
2023-09-21 03:06:00 -0700

    fixing error

--- _printf.c
+++ _printf.c
@@ -27,10 +27,10 @@ int _printf(const char *format, ...)
 		switch (*format)
 		{
 			case 'c':
-				printed_chars += print_char(ap);
+				printed_chars += custom_print_char(ap);
 				break;
 			case 's':
-				printed_chars += print_string(ap);
+				printed_chars += custom_print_string(ap);
 				break;
 			case '%':
 				printed_chars += print_percent(ap);
--- main.h
+++ main.h
@@ -4,8 +4,8 @@
 #include <stdarg.h>
 
 int _printf(const char *format, ...);
-int print_char(va_list ap);
-int print_string(va_list ap);
+int custom_print_char(va_list ap);
+int custom_print_string(va_list ap);
 int print_percent(va_list ap);
 
 #endif /* MAIN_H */
